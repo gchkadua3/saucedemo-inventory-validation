52bf63c486d9541ebbce7ecbab2c36b341f9c6a4
Giorgi Chkadua <cinna.madhouse@gmail.com>
2026-03-25 14:56:11 +0400

    add sortByPriceLowToHigh

--- src/pages/inventory.page.js
+++ src/pages/inventory.page.js
@@ -1,13 +1,18 @@
+const BasePage = require('./base.page');
+
 class InventoryPage extends BasePage {
 
-    
+    // Locator for the sort dropdown
     get dropDown() {
         return $("//select[@class='product_sort_container']");
     }
 
+    // Method to select "Price (low to high)"
     async sortByPriceLowToHigh() {
+        await this.dropDown.waitForDisplayed();           // wait until the dropdown is visible
         await this.dropDown.selectByVisibleText('Price (low to high)');
     }
+
 }
 
-module.exports = InventoryPage;
+module.exports = new InventoryPage();  // export instance for easy usage
--- src/tests/test.js
+++ src/tests/test.js
@@ -1,10 +1,20 @@
 const LoginPage = require('../pages/login.page');
+const InventoryPage = require('../pages/inventory.page');
 
 describe('Login Test', () => {
 
-    it('should login with valid credentials', async () => {
+    before(async () => {
+
         await LoginPage.open();
         await LoginPage.login('standard_user', 'secret_sauce');
+
+    });
+    
+
+    it('should sort items low to high', async () => {
+
+        await InventoryPage.sortByPriceLowToHigh();
+
     });
 
 
