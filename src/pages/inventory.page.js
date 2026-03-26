6db80765ceda4be4602df8c6c32e9675a13f0e81
Giorgi Chkadua <cinna.madhouse@gmail.com>
2026-03-26 10:34:51 +0400

    implement all tests

--- src/pages/inventory.page.js
+++ src/pages/inventory.page.js
@@ -6,13 +6,28 @@ class InventoryPage extends BasePage {
         return $("//select[@class='product_sort_container']");
     }
 
+    get addBackpack() {
+        return $("//button[@id='add-to-cart-sauce-labs-backpack']");
+    }
+
+    get addBikeLight() {
+        return $("//button[@id='add-to-cart-sauce-labs-bike-light']");
+    }
+
+    get removeBike(){
+        return $("//button[@id='remove-sauce-labs-bike-light']");
+    }
+
+    get cartBadge() {
+        return $("//span[@class='shopping_cart_badge']");
+    }
+
     async sortByPriceLowToHigh() {
         await this.dropDown.waitForDisplayed();
         await this.dropDown.selectByVisibleText('Price (low to high)');
     }
 
     async getItemPrices() {
-        
 
         const priceElements = Array.from(
             await browser.$$("//div[@class='inventory_item_price']")
@@ -21,6 +36,26 @@ class InventoryPage extends BasePage {
         const texts = await Promise.all(priceElements.map(el => el.getText()));
         return texts.map(text => parseFloat(text.replace('$', '')));
     }
+
+    async addTwoItemsToCart() {
+        await this.addBackpack.waitForDisplayed();
+        await this.addBackpack.click();
+        await this.addBikeLight.waitForDisplayed();
+        await this.addBikeLight.click();
+    }
+
+     async removeOneItemFromCart() {
+        await this.removeBike.waitForDisplayed();
+        await this.removeBike.click();
+    }
+
+    async getCartCount() {
+        await this.cartBadge.waitForDisplayed({ timeout: 3000 });
+        return await this.cartBadge.getText();
+    }
+
+    
+ 
 
 }
 
--- src/tests/test.js
+++ src/tests/test.js
@@ -21,4 +21,23 @@ describe('UC-1: Sorting Validation', () => {
         expect(prices).toEqual(sorted);
     });
 
+    it('should add two items to the cart and verify cart updates to 2', async () => {
+        await InventoryPage.addTwoItemsToCart();
+
+        const count = await InventoryPage.getCartCount();
+        console.log('DEBUG: Cart count:', count);
+
+        expect(count).toBe('2');
+    });
+
+      it('should remove one item and verify cart updates to 1', async () => {
+        await InventoryPage.removeOneItemFromCart();
+
+        const count = await InventoryPage.getCartCount();
+        console.log('DEBUG: Cart count after remove:', count);
+
+        expect(count).toBe('1');
+    });
+
+
 });
